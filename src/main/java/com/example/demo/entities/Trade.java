72cb4333427faf4f905974e6cedcbd669e0f7a03
Anmol <xyz@abc.com>
2021-08-27 05:55:54 +0000

    Fixed the ID 0 Error in SQL

--- src/main/java/com/example/demo/entities/Trade.java
+++ src/main/java/com/example/demo/entities/Trade.java
@@ -1,6 +1,8 @@
 package com.example.demo.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 public class Trade {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String stockTicker;
 	private double price;
