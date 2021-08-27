05ae0d3a263b40737da6dfcde0d9a3e1965523a0
GaneshAyyappan <ganesh@example.com>
2021-08-27 12:13:06 +0530

    added orderTime to schema.sql and trade.java

--- src/main/java/com/example/demo/entities/Trade.java
+++ src/main/java/com/example/demo/entities/Trade.java
@@ -1,5 +1,7 @@
 package com.example.demo.entities;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -14,19 +16,22 @@ public class Trade {
 	private double price;
 	private int volume;
 	private String buyOrSell;
+	private LocalDateTime orderTime; 
 	private int statusCode;
 
 	public Trade() {
-
+		super();
 	}
 
-	public Trade(int id, String stockTicker, double price, int volume, String buyOrSell, int statusCode) {
+	public Trade(int id, String stockTicker, double price, int volume, 
+			String buyOrSell, LocalDateTime orderTime, int statusCode) {
 		super();
 		this.id = id;
 		this.stockTicker = stockTicker;
 		this.price = price;
 		this.volume = volume;
 		this.buyOrSell = buyOrSell;
+		this.orderTime = orderTime;
 		this.statusCode = statusCode;
 	}
 
@@ -70,6 +75,15 @@ public class Trade {
 		this.buyOrSell = buyOrSell;
 	}
 
+
+	public LocalDateTime getOrderTime() {
+		return orderTime;
+	}
+
+	public void setOrderTime(LocalDateTime orderTime) {
+		this.orderTime = orderTime;
+	}
+
 	public int getStatusCode() {
 		return statusCode;
 	}
@@ -77,5 +91,5 @@ public class Trade {
 	public void setStatusCode(int statusCode) {
 		this.statusCode = statusCode;
 	}
-
+		
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,9 @@
-CREATE TABLE IF NOT EXISTS stocks (
+create table if not exists stocks(
 id int auto_increment primary key,
 stockTicker varchar(10) not null,
 price double not null,
 volume int not null,
-buyOrsell varchar(255) not null,
+buyOrsell varchar(5) not null,
+orderTime datetime default current_timestamp not null,
 statusCode int default 0
 );
-
-insert into stocks(stockTicker,price,volume,buyOrsell) values('ITC', 10, 50, 'buy');
-insert into stocks(stockTicker,price,volume,buyOrsell,statusCode) values('ZOMATO', 100, 50, 'sell', 0);
-insert into stocks(stockTicker,price,volume,buyOrsell,statusCode) values('ITC', 10, 25, 'sell', 0);
-insert into stocks(stockTicker,price,volume,buyOrsell) values('ZOMATO', 100, 20, 'buy');
